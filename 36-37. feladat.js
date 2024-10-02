f07fd38fc7fbe25de007f3309017bf8375410dbb
nemethkoppany <koppany344@gmail.com>
2024-10-02 17:07:16 +0200

    corrected mistakes

---
+++ 36-37. feladat.js
@@ -0,0 +1,17 @@
+const table = document.createElement('table');
+document.body.appendChild(table);
+
+const tr_1 = document.createElement('tr');
+table.appendChild(tr_1);
+
+const td_1 = document.createElement("td");
+tr_1.appendChild(td_1);
+
+const tr_2 = document.createElement('tr');
+table.appendChild(tr_2);
+
+const td_2 = document.createElement("td");
+tr_2.appendChild(td_2);
+
+const td_3 = document.createElement("td");
+tr_2.appendChild(td_3)
